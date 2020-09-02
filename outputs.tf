5adb58f2eb4ca795ea771c10c685baf83a8dac34
Maximilian Frank <maximilian.frank@ait.ac.at>
2020-09-02 12:39:45 +0200

    Add module outputs

---
+++ outputs.tf
@@ -0,0 +1,10 @@
+output "server" {
+    value = openstack_compute_instance_v2.server
+}
+
+output "additional_networks" {
+    value = {
+        for name, port in openstack_networking_port_v2.additional_port:
+        name => port
+    }
+}
